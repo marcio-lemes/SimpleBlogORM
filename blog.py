4c7c9aa86ae2b7053daa7b5400133e4fc209c381
unknown <marcinlemes4@gmail.com>
2025-03-21 12:13:25 -0300

    feat: add tratamento de erros nas funções e na conexão com BD.

--- blog.py
+++ blog.py
@@ -1,3 +1,4 @@
+from sqlalchemy.exc import IntegrityError
 from conexao_orm import Base, engine, session
 from user import User
 from post import Post
@@ -19,9 +20,17 @@ def add_user():
     name = input("Nome:\n")
     email = input("Email:\n")
     user = User(name, email) #Instancia a classe User que criamos com os atributos passados pelo usuário
-    session.add(user) #Adiciona o usuário no banco de dados (sem comandos SQL)
-    session.commit() #Realiza a mudança
-    print("Usuário adicionado com sucesso!")
+    try:
+        session.add(user) #Adiciona o usuário no banco de dados (sem comandos SQL)
+        session.commit() #Realiza a mudança
+        print("Usuário adicionado com sucesso!")
+    except IntegrityError:
+        session.rollback()
+        print("Erro: E-mail já cadastrado")
+    except Exception as e:
+        session.rollback()
+        print(f"Erro ao adicionar usuário: {e}")
+
 
 #Função para adicionar post  
 def add_post():
@@ -29,19 +38,25 @@ def add_post():
     title = input("Título:\n")
     content = input("Conteúdo:\n")
     author_id = input("Id do Autor:\n")
-    user = session.query(User).filter_by(id=author_id).first() #Verifica na tabela se o id passado pelo usuário em author_id existe
-    if user:
-        post = Post(title=title, content=content, author=user)
-        session.add(post)
-        session.commit()
-        print("Post adicionado com sucesso!")
-    else:
-        print("Usuário não encontrado.")
+    try:
+        user = session.query(User).filter_by(id=author_id).first() #Verifica na tabela se o id passado pelo usuário em author_id existe
+        if user:
+            post = Post(title=title, content=content, author=user)
+            session.add(post)
+            session.commit()
+            print("Post adicionado com sucesso!")
+        else:
+            print("Usuário não encontrado.")
+    except Exception as e:
+        print(f"Erro ao adicionar post: {e}")
         
 #Função para consultar usuários e posts
 def query_users_posts():
-    users = session.query(User).join(User.posts).order_by(User.name).all() #Realiza uma consulta na tabela "User", faz a junção com a tabela posts pelo relacionamento que existe na tabela "User" (User.posts), ordena pelo nome e por fim, solicita o retorno de todos os resultados (.all()) 
-    for user in users:
-        print(f"User: {user.name}, Email: {user.email}")
-        for post in user.posts:
-            print(f"Post: {post.title}, Conteúdo: {post.content}")
+    try:
+        users = session.query(User).join(User.posts).order_by(User.name).all() #Realiza uma consulta na tabela "User", faz a junção com a tabela posts pelo relacionamento que existe na tabela "User" (User.posts), ordena pelo nome e por fim, solicita o retorno de todos os resultados (.all()) 
+        for user in users:
+            print(f"User: {user.name}, Email: {user.email}")
+            for post in user.posts:
+                print(f"Post: {post.title}, Conteúdo: {post.content}")
+    except Exception as e:
+        print(f"Erro ao consultar usuários e posts: {e}")
